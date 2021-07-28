cc85526707b0b47b89a7279336aaf61a32787d36
VMS-Backend <shelcia1999@gmail.com>
2021-07-28 14:25:11 +0530

    ⚡️

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
           {"         "}
           by AUV IIITDM . Copyright © 2020. All Rights Reserved.
           <a href="https://www.privacypolicies.com/live/af693fba-7751-41fb-b826-ac1036dd0e85">
-            {"         "} Read our Privacy Policy
+            {"        "} Read our Privacy Policy
           </a>
         </p>
       </div>
